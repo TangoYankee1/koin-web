6a785c13919cbd7ee7bdd626ed598c62db3e8942
TangoYankee1 <aicsjohnrecanil@gmail.com>
2025-12-16 16:16:57 +0800

    modified:   src/api/axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://koin-api.onrender.com/api',
+    baseURL: 'https://koin-api.onrender.com:10000/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
